827a8bc0ba0bfd4f4cd944d4738e88da3c669de1
mariiatrokoz <62380981+mariiatrokoz@users.noreply.github.com>
2024-10-13 22:45:33 +0300

    Update temperature_converter.py

--- temperature_converter.py
+++ temperature_converter.py
@@ -6,9 +6,12 @@ unit = int(input('Press 1 for Celsius and 2 for Fahrenheit: '))
 def converter(t, unit):
     if unit == 1:  # Celsius to Fahrenheit
         output = str(t) + '°C is ' + str(9 * t / 5 + 32) + '°F'
-    else:  # Fahrenheit to Celsius
+    elif unit == 2:  # Fahrenheit to Celsius
         output = str(t) + '°F is ' + str((t - 32) * 5 / 9) + '°C'
+    else:
+        output = 'Sorry, only numbers 1 or 2 are allowed.'
     return output
 
 output = converter(t, unit)
 print(output)
+
